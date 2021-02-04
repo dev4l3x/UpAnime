c7e6e5fc5c01c13f66685cbde71be0e1241c3b81
alexsi <alexsi@ue.balidea.com>
2021-02-04 20:20:05 +0100

    Add dotenv and env file for database data

--- src/animescrapper/src/infraestructure/database/postgresAnimeRepository.js
+++ src/animescrapper/src/infraestructure/database/postgresAnimeRepository.js
@@ -1,11 +1,12 @@
 const AnimeEpisode = require('../../domain/animeEpisode');
+require('dotenv').config();
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'qwerty$1234',
-        database: 'upanime'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     },
 });
 
